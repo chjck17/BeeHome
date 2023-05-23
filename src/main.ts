7ce4e85be460c8567d90a1d6f0dd3213b4c7edf1
LAPTOP-8OI9JASM\19110 <19110181spkt@gmail.com>
2023-05-23 18:55:35 +0700

    auto

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestFastifyApplication } from '@nestjs/platform-fastify';
 import { FastifyAdapter } from '@nestjs/platform-fastify/adapters';
 import { contentParser } from 'fastify-multer';
-import { join } from 'path';
+import path, { join } from 'path';
+import express from 'express';
+
 import helmet from 'fastify-helmet';
 
 // import * as cookieParser from 'cookie-parser';
@@ -21,14 +23,9 @@ async function bootstrap() {
   );
   app.enableCors({ origin: '*', credentials: true });
   app.register(contentParser);
+  // app.useStaticAssets({ root: join(__dirname, '../../../BeeHome') });
 
-  // const path = require('path');
-
-  // app.use(
-  //   '/upload/single',
-  //   express.static(path.join(__dirname, '..', 'upload', 'single')),
-  // );
-  app.useStaticAssets({ root: join(__dirname, '..', 'upload', 'single') });
+  app.use('/upload', express.static(path.join(__dirname, '../../upload')));
   const config = new DocumentBuilder()
     .setTitle('BeeHome')
     .setDescription('BeeHome API')
